c44a93845c9dab4f23af0805ae325af0ca677b6f
sidd19898 <anganesiddhu@gmail.com>
2026-06-20 18:54:44 +0530

    minor fix for frontend deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-    baseURL: "http://localhost:3000/expense"
+    baseURL: "https://expense-tracking-app-7gh4.onrender.com/expense"
 
 });
 
